827606b6acc223982dd3d09b7e532d92ec366a8e
hapvskully <77029963+hapvskully@users.noreply.github.com>
2021-03-03 17:19:07 -0500

    Working on local storage buttons' functionality

--- Develop/script.js
+++ Develop/script.js
@@ -8,18 +8,20 @@ var time = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
 for(i=0; i<9; i++){
     var timeblock = $("<div>").attr("class", "row")
     
-    var newP = $("<p>").text(time[i]).attr("class", "col-1 hour")
+    var newP = $("<p>").text(time[i]).attr("class", "col-1 hour").attr("id",'time')
 
     if(parseInt(currentHour) > i+9) {
-        var newText = $("<textarea>").attr("class", "col-10 past")
+        var newText = $("<textarea>").attr("class", "col-10 past").attr("id",i)
     } else if(currentHour == i+9){
-        var newText = $("<textarea>").attr("class", "col-10 present")
+        var newText = $("<textarea>").attr("class", "col-10 present").attr("id",i)
     } else {
-        var newText = $("<textarea>").attr("class", "col-10 future")
+        var newText = $("<textarea>").attr("class", "col-10 future").attr("id",i)
     }
     JSON.stringify
     newText.text(localStorage.getItem(""))
     var newBtn = $("<button>").attr("class", "col-1 saveBtn").text("save")
+    // console.log(document.querySelector(`#hour-${i}`))
+    // console.log($(`#${i}`))
     newBtn.on("click", saveToLocal);
 
     timeblock.append(newP)
@@ -31,9 +33,11 @@ for(i=0; i<9; i++){
 
 function saveToLocal(){
     console.log("saved to local!")
-    var valueToSave = $(this).prev().val()
-    // localStorage.setItem("arr", JSON.stringify(time))
-    // JSON.parse(localStorage.getItem("arr"))
-    localStorage.setItem("textarea",)
-    localStorage.length("textarea",)
+
+    var EventName = document.querySelector('#time').id;
+    
+    var UserEvent = document.querySelector('textarea').value;
+
+    console.log(UserEvent)
+    window.localStorage.setItem(EventName, UserEvent)
 }
